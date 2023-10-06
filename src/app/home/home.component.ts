508e88818942b3579898bfb0769deba0f5d54325
Alessio-Tempesta <aletem999@gmail.com>
2023-10-06 17:42:32 +0200

    Houselocation Scaffolding

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    imports: [
+        CommonModule,
+        HousingLocationComponent
+    ]
+})
+export class HomeComponent {
+
+}
