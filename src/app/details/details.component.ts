3e7e99466be7ce03d88e382da36668fd65af1714
Alessio-Tempesta <aletem999@gmail.com>
2023-10-07 13:50:44 +0200

    Personalizzato la pagina dei dettagli

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
 
-}
+  export class DetailsComponent {
+
+    route: ActivatedRoute = inject(ActivatedRoute);
+    housingService = inject(HousingService);
+    housingLocation: Housinglocation | undefined;
+
+    constructor() {
+      const housingLocationId = Number(this.route.snapshot.params['id']);
+      this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    }
+
+  }
+
